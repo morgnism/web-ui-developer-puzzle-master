2bd4c40323fba12af02cf2936064ebd26a8b5ee5
morganistic <morgan@border.dev>
2021-03-19 12:18:14 -0600

    added instant search

--- libs/books/feature/src/lib/book-search-input/book-search-input.component.ts
+++ libs/books/feature/src/lib/book-search-input/book-search-input.component.ts
@@ -1,24 +1,28 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  Input,
-  Output,
-} from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, Output } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { merge, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 
 @Component({
   selector: 'tmo-book-search-input',
   templateUrl: 'book-search-input.component.html',
   styleUrls: ['./book-search-input.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BookSearchInputComponent {
-  @Input() searchForm: FormGroup;
+  searchForm = this.fb.group({
+    term: '',
+  });
 
-  @Output() search = new EventEmitter();
+  search$ = new Subject<void>();
 
-  searchBooks() {
-    this.search.emit();
-  }
+  @Output() search = merge(
+    this.search$.asObservable(),
+    this.searchForm.get('term').valueChanges
+  ).pipe(
+    debounceTime(500),
+    distinctUntilChanged(),
+    map(() => this.searchForm.value.term)
+  );
+
+  constructor(private readonly fb: FormBuilder) {}
 }
--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -1,50 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {
   addToReadingList,
   clearSearch,
   getAllBooks,
-  ReadingListBook,
   searchBooks,
 } from '@tmo/books/data-access';
-import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
-import { Observable } from 'rxjs';
-import { pluck } from 'rxjs/operators';
 
 @Component({
   selector: 'tmo-book-search',
   templateUrl: './book-search.component.html',
 })
-export class BookSearchComponent implements OnInit {
-  searchForm = this.fb.group({
-    term: '',
-  });
+export class BookSearchComponent {
+  searchTerm: string;
+  books$ = this.store.select(getAllBooks);
 
-  books$: Observable<ReadingListBook[]>;
-  searchTerm$ = this.searchForm.valueChanges.pipe(pluck('term'));
-
-  constructor(
-    private readonly store: Store,
-    private readonly fb: FormBuilder
-  ) {}
-
-  ngOnInit(): void {
-    this.books$ = this.store.select(getAllBooks);
-  }
+  constructor(private readonly store: Store) {}
 
   addBookToReadingList(book: Book) {
     this.store.dispatch(addToReadingList({ book }));
   }
 
-  searchExample() {
-    this.searchForm.controls.term.setValue('javascript');
-    this.searchBooks();
-  }
-
-  searchBooks() {
-    if (this.searchForm.value.term) {
-      this.store.dispatch(searchBooks({ term: this.searchForm.value.term }));
+  searchBooks(term: string) {
+    // `combineLatest` doesn't emit until all observables complete
+    // while `merge` emits twice, once immediately for the enter skipping the map entirely
+    // this is to prevent KeyboardEvent from querying
+    if (typeof term !== 'string') {
+      return;
+    }
+    this.searchTerm = term;
+    if (term) {
+      this.store.dispatch(searchBooks({ term }));
     } else {
       this.store.dispatch(clearSearch());
     }
